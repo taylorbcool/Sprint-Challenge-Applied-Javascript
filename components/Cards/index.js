0134b348828b123ac789f2d6440808d3fe3b821d
Taylor Cool <taycool@gmail.com>
2019-11-01 09:34:49 -0700

    finished articles/cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,44 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then((response) => {
+        Object.values(response.data.articles).forEach((arr) => {
+            arr.forEach((item) => {
+                createArticle(item);
+            });
+        })
+    })
+    .catch((error) => {
+        console.log('Could not fetch articles; ' + error);
+    });
+
+function createArticle(object) {
+    const 
+        articlesContainer = document.querySelector('.cards-container'),
+        article = document.createElement('div'),
+        articleHeadline = document.createElement('div'),
+        articleAuthor = document.createElement('div'),
+        authorImgContainer = document.createElement('div'),
+        authorImg = document.createElement('img'),
+        articleAuthorName = document.createElement('span');
+
+    articleAuthorName.textContent = `By ${object.authorName}`;
+    authorImg.src = object.authorPhoto;
+    articleHeadline.textContent = object.headline;
+
+    article.classList.add('card');
+    articleHeadline.classList.add('headline');
+    articleAuthor.classList.add('author');
+    authorImgContainer.classList.add('img-container');
+
+    articlesContainer.appendChild(article);
+    article.appendChild(articleHeadline);
+    article.appendChild(articleAuthor);
+    articleAuthor.appendChild(authorImgContainer);
+    articleAuthor.appendChild(articleAuthorName);
+    authorImgContainer.appendChild(authorImg);
+
+    return article;
+}
